1a1cbf2b08059db984db1ae550defc768e81c3bf
Pardosa13 <j.partington13@hotmail.com>
2025-11-20 20:42:00 +1100

    Update app.py

--- app.py
+++ app.py
@@ -254,5 +254,5 @@ def init_db():
             print(f'Admin user already exists: {admin_username}')
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
